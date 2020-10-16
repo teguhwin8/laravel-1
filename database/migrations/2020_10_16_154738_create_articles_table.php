9ade96bb56cbdec678cdef0e7aa5f22ecf42a72e
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2020-10-16 15:50:44 +0700

    migration create_articles_table

---
+++ database/migrations/2020_10_16_154738_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 255);
+            $table->string('title', 255);
+            $table->text('subject');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
